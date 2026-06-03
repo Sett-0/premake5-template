549d38fa278a9b4b47b6625091c1015a5b40fd14
Sett-0 <ampilov2000@mail.ru>
2026-06-03 17:07:25 +0300

    Add template code

---
+++ build/premake5.lua
@@ -0,0 +1,37 @@
+workplaceName = "workplaceName"
+projectName   = "projectName"
+
+workspace (workplaceName)
+	configurations { "Debug", "Release" }
+	platforms { "x64" }
+	location "../"
+    targetdir "../bin/%{cfg.buildcfg}"
+	
+	filter "configurations:Debug"
+		defines { "DEBUG" }
+		symbols "On"
+	filter "configurations:Release"
+		defines { "NDEBUG" }
+		optimize "On"
+	filter {}
+	
+project (projectName)
+	kind "ConsoleApp"
+	language "C++"
+	cppdialect "C++17"
+	
+	-- files { "../main.cpp" }
+	-- includedirs { "../include" }
+	-- libdirs { "../lib" }
+	-- links { "raylib", "gdi32", "winmm" }
+
+-- Creating 'make run' command
+local wks_gen = premake.modules.gmake.generate_workspace
+premake.modules.gmake.generate_workspace = function(wks)
+	-- Do make as usual
+    wks_gen(wks)
+    
+	-- Add run command 
+    premake.w("\nrun: all")
+    premake.w("@bin/$(if $(findstring release,$(config)),Release,Debug)/" .. projectName .. ".exe")
+end
---
+++ run_premake.bat
@@ -0,0 +1,4 @@
+cd build
+premake5.exe gmake
+cd ..
+mingw32-make clean
